9b2b2ce660538954bd8ef42025135ec46b7d86c4
DeejMags <rnmaglatangjr@gmail.com>
2024-11-29 09:45:36 +0800

    Update: Gradle and Libs are sync the nav_header.xml still working on it and the DataBinderMapping still has an error though

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,23 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.cardview)
-    implementation(libs.androidx.appcompat.v161)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    }
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.activity:activity:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("androidx.databinding:databinding-runtime:7.0.0")
+}
 
 
